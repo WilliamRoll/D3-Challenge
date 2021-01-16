75eed0466966be63fb1263723aab55f445f0fe31
WilliamRoll <broll6595@gmail.com>
2021-01-16 15:41:24 -0500

    scatter plot is fully functioning, displaying state abbr in circles

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -27,15 +27,16 @@ d3.csv("assets/data/data.csv").then(function(dataVar) {
     dataVar.forEach(function(data){
       data.healthcare = +data.healthcare;
       data.poverty = +data.poverty;
+      data.abbr = data.abbr;
     });
   
   //Create linear scale functions
   var xLinearScale = d3.scaleLinear()
-    .domain([9, d3.max(dataVar, d => d.poverty)])
+    .domain([8.5, d3.max(dataVar, d => d.poverty)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
-    .domain([4, d3.max(dataVar, d => d.healthcare)])
+    .domain([3.9, d3.max(dataVar, d => d.healthcare)])
     .range([height, 0]);
   
   //Create axis functions 
@@ -51,15 +52,43 @@ d3.csv("assets/data/data.csv").then(function(dataVar) {
     .call(leftAxis);
 
   //Create circles for the states
-  var circlesGroup = chartGroup.selectAll("circle")
+  chartGroup.selectAll("circle")
+    .data(dataVar)
+    .enter()
+    .append("text")
+    .text(d => d.abbr)
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "10px")
+    .attr("text-anchor", "middle")
+    .attr("fill", "black");
+
+
+  chartGroup.selectAll("Circle")
     .data(dataVar)
     .enter()
     .append("circle")
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
-    .attr("r", "15")
+    .attr("r", 10)
     .attr("fill", "blue")
-    .attr("opacity", ".5");
+    .attr("opacity", "0.3");
 
+
+
+    // Create axes labels
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
   
 });
